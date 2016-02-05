6900b90612b8a6d863eb406cb4e93840a70787d9
Antoni Virós Martín <antoni.viros@gmail.com>
2016-02-06 00:45:08 +0100

    Functionality done

--- js/index.js
+++ js/index.js
@@ -58,7 +58,6 @@ $(document).ready(function() {
       // Code to run if the request fails; the raw request and
       // status codes are passed to the function
       error: function(xhr, status, errorThrown) {
-          alert("Sorry, there was a problem!");
           console.log("Error: " + errorThrown);
           console.log("Status: " + status);
           console.dir(xhr);
